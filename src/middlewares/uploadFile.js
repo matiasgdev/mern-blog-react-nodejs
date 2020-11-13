1a5ddd9b09a164322147aeb149fc5faaac61ff45
matiasgdev <matiasg.developer@gmail.com>
2020-11-13 16:01:17 -0300

    fixing public folder

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -3,9 +3,14 @@ import path from 'path'
 import { nanoid } from 'nanoid'
 
 
+let imageRoute = ''
+if (process.env.NODE_ENV === 'production') {
+  imageRoute = 'https://blog-mern-stack-matiasgdev.herokuapp.com/'
+}
+
 const manageStorage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, 'public/images')
+    cb(null, imageRoute + 'public/images')
   },
   filename: function(req, file, cb) {
     let fileName = nanoid(5) + path.extname(file.originalname)
